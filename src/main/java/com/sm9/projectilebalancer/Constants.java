96b7c432ba86e1de4dca096ee27dd01180504955
SM9CC <me@sm9.dev>
2019-06-04 22:22:20 +0100

    Workspace stuff, Gradle upgrade, Ask to load after all mods nicely.

--- src/main/java/com/sm9/projectilebalancer/Constants.java
+++ src/main/java/com/sm9/projectilebalancer/Constants.java
@@ -5,6 +5,5 @@ class Constants {
     public static final String MOD_NAME = "Projectile Balancer";
     public static final String MOD_VERSION = "0.3";
     public static final String MINECRAFT_VERSION = "1.12.2";
-    private static final String MIN_FORGE_VER = "14.23.5.2836";
-    public static final String DEPENDENCIES = "after:forge@[" + MIN_FORGE_VER + ",)";
+    static final String DEPENDENCIES = "after:forge@[" + "14.23.5.2838" + ",)" + ";after:*";
 }
